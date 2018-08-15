3016b62b804f6f07cfe7cb2a46c086461871c04d
Dariel Rivero <drivero@nisum.com>
2018-08-15 09:37:00 -0300

    Including avl tests

--- src/test/java/drll/dataStructures/tree/avl/AVLTreeTest.java
+++ src/test/java/drll/dataStructures/tree/avl/AVLTreeTest.java
@@ -9,7 +9,10 @@ import drll.dataStructures.tree.avl.AVLTree;
 import drll.dataStructures.tree.avl.BalanceFactor;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AVLTreeTest {
 
     private AVLTree<Integer> tree;
