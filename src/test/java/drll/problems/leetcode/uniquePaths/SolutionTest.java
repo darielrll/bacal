bcd3a990bf18c1aaa18686c82196539775e38907
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2021-04-28 21:51:49 -0400

    Add solution for problem Unique Paths

---
+++ src/test/java/drll/problems/leetcode/uniquePaths/SolutionTest.java
@@ -0,0 +1,20 @@
+package drll.problems.leetcode.uniquePaths;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+    private Solution solution;
+
+    @BeforeEach
+    public void init(){
+        solution = new Solution();
+    }
+
+    @Test
+    public void should_return_correctly_for_test_case1() {
+        assertThat(solution.uniquePaths(3, 7)).isEqualTo(28);
+    }
+}
