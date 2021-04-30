865723efa759e5769c717de0cb674a3135fa7c6e
d0r019w <dariel.rivero@walmart.com>
2021-04-30 12:47:23 -0400

    Add problem Hotel construction description - pending solution

---
+++ src/main/java/drll/problems/hackerRank/hotelConstruction/Solution.java
@@ -0,0 +1,9 @@
+package drll.problems.hackerRank.hotelConstruction;
+
+import java.util.List;
+
+public class Solution {
+    public int numberOfWays(List<List<Integer>> roads) {
+        return -1;
+    }
+}
