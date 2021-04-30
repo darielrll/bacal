2841a4ba991aa30f1295f123fba811004718f833
d0r019w <dariel.rivero@walmart.com>
2021-04-30 13:29:08 -0400

    Add problem Equalizing Array Elements description - pending solution

---
+++ src/main/java/drll/problems/hackerRank/equalizingArrayElements/Solution.java
@@ -0,0 +1,9 @@
+package drll.problems.hackerRank.equalizingArrayElements;
+
+import java.util.List;
+
+public class Solution {
+    public int minOperations(List<Integer> arr, int threshold, int d) {
+        return -1;
+    }
+}
