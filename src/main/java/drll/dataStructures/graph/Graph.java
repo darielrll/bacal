8e085e103bbe4ef424e50c9b3b2ecf34c50f7ac8
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2021-05-04 22:28:52 -0400

    Graph implementation completed

--- src/main/java/drll/dataStructures/graph/Graph.java
+++ src/main/java/drll/dataStructures/graph/Graph.java
@@ -19,34 +19,6 @@ public class Graph {
         addEdge(edges);
     }
 
-    private List<List<Integer>> initAdjacencyList(int vertex) {
-        List<List<Integer>> adjacencyList = new ArrayList<>(vertex);
-        for (int i = 0; i <= vertex; i++) {
-            adjacencyList.add(new ArrayList<>());
-        }
-        return adjacencyList;
-    }
-
-    private int calculateVertexCount(int[][] edges) {
-        int lastVertex = 0;
-        for (int i = 0; i < edges.length; i++) {
-            for (int j = 0; j < edges[i].length; j++) {
-                if(lastVertex < edges[i][j]){
-                    lastVertex = edges[i][j];
-                }
-            }
-        }
-        return lastVertex;
-    }
-
-    private int calculateEdgesCount(List<List<Integer>> edges) {
-        int edgesCount = 0;
-        for (List<Integer> adyacents : edges) {
-            edgesCount += adyacents.size();
-        }
-        return edgesCount;
-    }
-
     public int vertexCount() {
         return adjacencyList.size();
     }
@@ -68,12 +40,9 @@ public class Graph {
             return false;
         }
         adjacencyList.get(v1).add(v2);
+        adjacencyList.get(v2).add(v1);
         edgesCount++;
         return true;
-    }
-
-    private boolean isNotValidEdge(int v1, int v2) {
-        return v1 < 0 || v2 < 0 || v1 >= adjacencyList.size() || v2 >= adjacencyList.size();
     }
 
     public void addEdge(int[][] edges) {
@@ -83,5 +52,49 @@ public class Graph {
         for (int i = 0; i < edges.length; i++) {
             addEdge(edges[i][0], edges[i][1]);
         }
+    }
+
+    public List<Integer> adjacent(int vertex) {
+        if(isNotValidVertex(vertex)){
+            throw new InvalidParameterException(String.format("Vertex indexes should be >= 0 and <= %s",
+                    adjacencyList.size() - 1));
+        }
+        return adjacencyList.get(vertex);
+    }
+
+    public int vertexDegree(int vertex) {
+        if(isNotValidVertex(vertex)){
+            throw new InvalidParameterException(String.format("Vertex indexes should be >= 0 and <= %s",
+                    adjacencyList.size() - 1));
+        }
+        return adjacencyList.get(vertex).size();
+    }
+
+    private boolean isNotValidEdge(int v1, int v2) {
+        return isNotValidVertex(v1)  ||  isNotValidVertex(v2);
+    }
+
+    private boolean isNotValidVertex(int vertex) {
+        return vertex < 0 || vertex >= adjacencyList.size();
+    }
+
+    private List<List<Integer>> initAdjacencyList(int vertex) {
+        List<List<Integer>> adjacencyList = new ArrayList<>(vertex);
+        for (int i = 0; i <= vertex; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+        return adjacencyList;
+    }
+
+    private int calculateVertexCount(int[][] edges) {
+        int lastVertex = 0;
+        for (int i = 0; i < edges.length; i++) {
+            for (int j = 0; j < edges[i].length; j++) {
+                if(lastVertex < edges[i][j]){
+                    lastVertex = edges[i][j];
+                }
+            }
+        }
+        return lastVertex;
     }
 }
--- src/test/java/drll/dataStructures/graph/GraphTest.java
+++ src/test/java/drll/dataStructures/graph/GraphTest.java
@@ -3,6 +3,7 @@ package drll.dataStructures.graph;
 import org.junit.jupiter.api.Test;
 
 import java.security.InvalidParameterException;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -26,18 +27,23 @@ public class GraphTest {
     @Test
     public void should_have_one_edge_when_graph_is_created_with_one_edge(){
         int[][] edges = new int[][]{{0, 2}};
+
         Graph graph = new Graph(edges);
 
         assertThat(graph.edgeCount()).isEqualTo(1);
         assertThat(graph.isAdjacent(0, 2)).isTrue();
+        assertThat(graph.isAdjacent(2, 0)).isTrue();
     }
 
     @Test
     public void should_be_adjacent_v1_and_v2_when_graph_is_created_with_edge_1_and_2(){
         int[][] edges = new int[][]{{1, 2}};
+
         Graph graph = new Graph(edges);
 
+        assertThat(graph.edgeCount()).isEqualTo(1);
         assertThat(graph.isAdjacent(1, 2)).isTrue();
+        assertThat(graph.isAdjacent(2, 1)).isTrue();
     }
 
     @Test
@@ -46,6 +52,8 @@ public class GraphTest {
         Graph graph = new Graph(edges);
 
         assertThat(graph.addEdge(1, 2)).isFalse();
+        assertThat(graph.addEdge(2, 1)).isFalse();
+        assertThat(graph.edgeCount()).isEqualTo(1);
     }
 
     @Test
@@ -55,16 +63,33 @@ public class GraphTest {
 
         assertThat(graph.addEdge(0, 2)).isTrue();
         assertThat(graph.isAdjacent(0, 2)).isTrue();
+        assertThat(graph.isAdjacent(2, 0)).isTrue();
+        assertThat(graph.edgeCount()).isEqualTo(2);
     }
 
     @Test
-    public void should_return_add_a_list_of_edge(){
+    public void should_add_a_list_of_edge(){
         int[][] edges = new int[][]{{1, 2}, {0, 2}, {0, 3}};
         Graph graph = new Graph(4);
 
         graph.addEdge(edges);
 
+        assertThat(graph.isAdjacent(1, 2)).isTrue();
+        assertThat(graph.isAdjacent(2, 1)).isTrue();
+        assertThat(graph.isAdjacent(0, 2)).isTrue();
+        assertThat(graph.isAdjacent(2, 0)).isTrue();
+        assertThat(graph.isAdjacent(0, 3)).isTrue();
+        assertThat(graph.isAdjacent(3, 0)).isTrue();
         assertThat(graph.edgeCount()).isEqualTo(3);
+    }
+
+    @Test
+    public void should_not_add_a_null_list_of_edges(){
+        Graph graph = new Graph(4);
+
+        graph.addEdge(null);
+
+        assertThat(graph.edgeCount()).isEqualTo(0);
     }
 
     @Test
@@ -82,5 +107,44 @@ public class GraphTest {
 
         InvalidParameterException exception = assertThrows(InvalidParameterException.class, () -> graph.addEdge(2, 6));
         assertThat("Vertex indexes should be >= 0 and <= 1").isEqualTo(exception.getMessage());
+    }
+
+    @Test
+    public void should_have_vertex_2_degree_3(){
+        int[][] edges = new int[][]{{1, 2}, {0, 2}, {2, 3}};
+        Graph graph = new Graph(edges);
+
+        assertThat(graph.vertexDegree(2)).isEqualTo(3);
+    }
+
+    @Test
+    public void should_throw_exception_when_is_request_vertex_degree_for_an_invalid_vertex(){
+        int[][] edges = new int[][]{{1, 2}, {0, 2}, {2, 3}};
+        Graph graph = new Graph(edges);
+
+        InvalidParameterException exception = assertThrows(InvalidParameterException.class, () -> graph.vertexDegree(7));
+        assertThat("Vertex indexes should be >= 0 and <= 3").isEqualTo(exception.getMessage());
+    }
+
+    @Test
+    public void should_have_three_adjacent_vertex(){
+        int[][] edges = new int[][]{{1, 2}, {0, 2}, {2, 3}};
+        Graph graph = new Graph(edges);
+
+        List<Integer> adjacents = graph.adjacent(2);
+
+        assertThat(adjacents.size()).isEqualTo(3);
+        assertThat(adjacents.contains(1)).isTrue();
+        assertThat(adjacents.contains(0)).isTrue();
+        assertThat(adjacents.contains(3)).isTrue();
+    }
+
+    @Test
+    public void should_throw_exception_when_is_request_adjacent_for_an_invalid_vertex(){
+        int[][] edges = new int[][]{{1, 2}, {0, 2}, {2, 3}};
+        Graph graph = new Graph(edges);
+
+        InvalidParameterException exception = assertThrows(InvalidParameterException.class, () -> graph.adjacent(7));
+        assertThat("Vertex indexes should be >= 0 and <= 3").isEqualTo(exception.getMessage());
     }
 }
