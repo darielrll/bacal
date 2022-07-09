eb7792c13c9676913484d04f73b520eecbe895c1
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2022-07-09 12:07:39 -0400

    WIP BWC-82

---
+++ src/main/java/drll/problems/leetCodeContest/problem1/Solution.java
@@ -0,0 +1,29 @@
+package drll.problems.leetCodeContest.problem1;
+
+public class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if(root.val == 0){
+            return false;
+        }
+        if(root.val == 1){
+            return true;
+        }
+        if(root.val == 2){
+            return evaluateTree(root.left)  ||  evaluateTree(root.right);
+        }
+        return evaluateTree(root.left)  &&  evaluateTree(root.right);
+    }
+
+    static public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+}
---
+++ src/main/java/drll/problems/leetCodeContest/problem2/Solution.java
@@ -0,0 +1,7 @@
+package drll.problems.leetCodeContest.problem2;
+
+public class Solution {
+    public int search(int target) {
+        return -1;
+    }
+}
