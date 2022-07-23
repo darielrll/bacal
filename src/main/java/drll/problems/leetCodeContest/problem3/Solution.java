51c91fb55f789788bfd7061b6fe05c24d275c0ab
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2022-07-23 14:00:16 -0400

    WIP Solution for problems 1 and 3

---
+++ src/main/java/drll/problems/leetCodeContest/problem3/Solution.java
@@ -0,0 +1,93 @@
+package drll.problems.leetCodeContest.problem3;
+
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public Object[] testNumberContainer(String[] instructions, int[][] parameters) {
+        NumberContainers numberContainers = new NumberContainers();
+        Object[] response = new Object[instructions.length];
+        response[0] = null;
+        for (int i = 1; i < instructions.length; i++) {
+            if ("find".equals(instructions[i])) {
+                response[i] = numberContainers.find(parameters[i][0]);
+            } else {
+                numberContainers.change(parameters[i][0], parameters[i][1]);
+                response[i] = null;
+            }
+        }
+        return response;
+    }
+}
+
+class NumberContainers {
+    private final HashMap<Integer, Integer> indexMap;
+    private final HashMap<Integer, PriorityQueue<Integer>> numberMap;
+
+    public NumberContainers() {
+        indexMap = new HashMap<>();
+        numberMap = new HashMap<>();
+    }
+
+    public int find(int number) {
+        if(numberMap.containsKey(number)){
+            return numberMap.get(number).peek();
+        }
+        return -1;
+    }
+
+    public void change(int index, int number) {
+        if(indexMap.containsKey(index)){
+            replaceIndex(index, number);
+        }
+        else {
+            addNewIndex(index, number);
+        }
+    }
+
+    private void addNewIndex(int index, int number) {
+        PriorityQueue<Integer> indexes = getIntegerPriorityQueue(number);
+        indexes.add(index);
+        numberMap.putIfAbsent(number, indexes);
+        indexMap.put(index, number);
+    }
+
+    private void replaceIndex(int index, int number) {
+        removeIndexFromNumber(index);
+        updatePairNumberIndex(index, number);
+
+        indexMap.replace(index, number);
+    }
+
+    private void updatePairNumberIndex(int index, int number) {
+        PriorityQueue<Integer> indexes = getIntegerPriorityQueue(number);
+
+        if(numberMap.containsKey(number)){
+            indexes.remove(index);
+        }
+        if(!indexes.contains(index)){
+            indexes.add(index);
+        }
+
+        numberMap.putIfAbsent(number, indexes);
+    }
+
+    private void removeIndexFromNumber(int index) {
+        Integer numberToBeReplaced = indexMap.get(index);
+        PriorityQueue<Integer> indexes = numberMap.get(numberToBeReplaced);
+        indexes.remove(index);
+
+        if (indexes.isEmpty()){
+            numberMap.remove(numberToBeReplaced);
+        }
+        else {
+            numberMap.replace(numberToBeReplaced, indexes);
+        }
+    }
+
+    private PriorityQueue<Integer> getIntegerPriorityQueue(int number) {
+        return numberMap.containsKey(number)
+                ? numberMap.get(number)
+                : new PriorityQueue<>();
+    }
+}
