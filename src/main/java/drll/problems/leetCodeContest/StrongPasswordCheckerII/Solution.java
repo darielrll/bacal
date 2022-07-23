8f6a1e49e12c6975c211fb757593de3b6e7acdd0
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2022-07-23 19:09:36 -0400

    Rename folders and files with the names of the problems

--- src/main/java/drll/problems/leetCodeContest/StrongPasswordCheckerII/Solution.java
+++ src/main/java/drll/problems/leetCodeContest/StrongPasswordCheckerII/Solution.java
@@ -1,4 +1,4 @@
-package drll.problems.leetCodeContest.problem1;
+package drll.problems.leetCodeContest.StrongPasswordCheckerII;
 
 public class Solution {
     public boolean strongPasswordCheckerII(String password) {
