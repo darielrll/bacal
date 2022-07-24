a368960b1b1af889206421263a637c42574013f3
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2022-07-24 00:23:15 -0400

    Add solution for solved problem in wc-300 and update metadata files

---
+++ src/main/java/drll/problems/leetcode/DecodeTheMessage/DecodeTheMessage.md
@@ -0,0 +1,14 @@
+## Link to the problem
+ 
+ - https://leetcode.com/contest/weekly-contest-300/problems/decode-the-message/
+ 
+##### Difficulty Level (according to Leetcode)
+ 
+ > Easy ( String )
+ 
+##### Resume of the problem:
+
+
+
+##### Notes
+  
---
+++ src/main/java/drll/problems/leetcode/DecodeTheMessage/Solution.java
@@ -0,0 +1,33 @@
+package drll.problems.leetcode.DecodeTheMessage;
+
+import java.util.HashMap;
+
+public class Solution {
+    public String decodeMessage(String key, String message) {
+        StringBuilder decodedMessage = new StringBuilder();
+
+        HashMap<Character, Character> table = new HashMap<>();
+        char firstLetter = 97;
+        for (int i = 0; i < key.length(); i++) {
+            Character character = key.charAt(i);
+            if(!character.equals(' ')  &&  !table.containsKey(character)){
+                table.put(character, firstLetter);
+                firstLetter++;
+            }
+            if(firstLetter > 122){
+                break;
+            }
+        }
+
+        for (int i = 0; i < message.length(); i++) {
+            if(message.charAt(i) == ' '){
+                decodedMessage.append(' ');
+            }
+            else {
+                decodedMessage.append(table.get(message.charAt(i)));
+            }
+        }
+
+        return decodedMessage.toString();
+    }
+}
