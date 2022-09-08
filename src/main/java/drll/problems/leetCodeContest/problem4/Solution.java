fcedfb85b8cfc4ab94ccc77af569320f1f24d5fb
Dariel Rivero <dariel.rivero@walmart.com>
2022-09-08 12:09:04 -0400

    WIP implementing a segment tree to solve the hard problem

--- src/main/java/drll/problems/leetCodeContest/problem4/Solution.java
+++ src/main/java/drll/problems/leetCodeContest/problem4/Solution.java
@@ -2,9 +2,10 @@ package drll.problems.leetCodeContest.problem4;
 
 public class Solution {
     public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
-        long[] subSums = subSums(runningCosts);
-        // int[] maxCharges = subMax(chargeTimes);
-        Node[] maxSegmentTree = getMaxSegmentTree(chargeTimes);
+        long[] subSums = buildConsecutiveSums(runningCosts);
+
+        int[] a = new int[]{-1, 2, 5, 0};
+        SegmentTree tree = new SegmentTree(a);
 
         int maxConsecutive = 0;
         for (int k = 1; k <= chargeTimes.length - 1; k++) {
@@ -13,13 +14,9 @@ public class Solution {
             boolean findNextK = false;
             while (windowEnd <= chargeTimes.length - 1){
 
-                int maxCharge = getMaxForceBrute(chargeTimes, windowStart, windowEnd);
-                int maxCharge2 = getMaxInSegment(maxSegmentTree[windowStart], maxSegmentTree[windowEnd]);
+//                int maxCharge = getMaxForceBrute(chargeTimes, windowStart, windowEnd);
+                int maxCharge = tree.getMax(windowStart, windowEnd);
                 long sum = getSubSum(subSums, windowStart, windowEnd);
-
-                if(maxCharge != maxCharge2){
-                    maxCharge2 = getMaxInSegment(maxSegmentTree[windowStart], maxSegmentTree[windowEnd]);
-                }
 
                 long totalCost = maxCharge + k * sum;
                 if(totalCost <= budget){
@@ -38,13 +35,6 @@ public class Solution {
 
 
         return maxConsecutive;
-    }
-
-    private int getMaxInSegment(Node start, Node end) {
-        if(start.equals(end)){
-            return start.value;
-        }
-        return getMaxInSegment(start.parent, end.parent);
     }
 
     private int getMaxForceBrute(int[] chargeTimes, int windowStart, int windowEnd) {
@@ -76,7 +66,7 @@ public class Solution {
         return subSums[windowEnd + 1] - subSums[windowStart - 1] - (subSums[windowEnd + 1] - subSums[windowEnd]);
     }
 
-    private long[] subSums(int[] numbers){
+    private long[] buildConsecutiveSums(int[] numbers){
         long[] subSums = new long[numbers.length];
         subSums[0] = numbers[0];
         for (int i = 1; i < numbers.length; i++) {
@@ -84,57 +74,68 @@ public class Solution {
         }
         return subSums;
     }
-
-    private int[] subMax(int[] numbers) {
-        int[] subMax = new int[numbers.length];
-        subMax[0] = numbers[0];
-        int max = subMax[0];
-        for (int i = 1; i < numbers.length; i++) {
-            if (max < numbers[i]) {
-                max = numbers[i];
-            }
-            subMax[i] = max;
-        }
-        return subMax;
-    }
-
-    private Node[] getMaxSegmentTree(int[] numbers){
-        Node[] maxSegments = new Node[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            maxSegments[i] = new Node(numbers[i]);
-        }
-        buildTreeBottomUp(maxSegments);
-        return maxSegments;
-    }
-
-    private void buildTreeBottomUp(Node[] level){
-        if(level.length == 1){
-            return;
-        }
-        Node[] levelUp = new Node[(level.length / 2) + (level.length % 2 == 0 ? 0 : 1)];
-        int positionCovered = 0;
-        for (int i = 0; i < level.length; i += 2) {
-            Node parent;
-            if( i + 1 < level.length){
-                parent = new Node(Math.max(level[i].value, level[i + 1].value));
-                level[i + 1].parent = parent;
-            }
-            else{
-                parent = new Node(level[i].value);
-            }
-            level[i].parent = parent;
-            levelUp[positionCovered] = parent;
-            positionCovered++;
-        }
-        buildTreeBottomUp(levelUp);
-    }
 }
 
-class Node{
-    int value;
-    Node parent;
+class SegmentTree{
+    private SegmentTreeNode[] tree;
 
-    public Node(int number) {
-        value = number;
+    public SegmentTree(int[] numbers) {
+        tree = new SegmentTreeNode[2 * numbers.length ];
+        buildTree(tree, numbers, 1, 0, numbers.length - 1);
+    }
+
+    private void buildTree(SegmentTreeNode[] tree, int[] numbers, int nodePosition, int leftMost, int rightMost) {
+        if(leftMost == rightMost){
+            tree[nodePosition] = new SegmentTreeNode(leftMost, rightMost, numbers[leftMost]);
+        }
+        else{
+            int leftChildPosition = 2 * nodePosition;
+            int leftChildRightMost = (leftMost + rightMost) / 2;
+            buildTree(tree, numbers, leftChildPosition, leftMost, leftChildRightMost);
+
+            int rightChildPosition = 2 * nodePosition + 1;
+            int rightChildLeftMost = (leftMost + rightMost) / 2 + 1;
+            buildTree(tree, numbers, rightChildPosition, rightChildLeftMost, rightMost);
+
+            int nodeValue = Math.max(tree[leftChildPosition].max, tree[rightChildPosition].max);
+            tree[nodePosition] = new SegmentTreeNode(leftMost, rightMost, nodeValue);
+        }
+    }
+
+    public int getMax(int i, int j) {
+        return getMax(1, i, j);
+    }
+
+    private int getMax(int nodePosition, int leftInterval, int rightInterval) {
+        if(isNodeSubsetOfInterval(nodePosition, leftInterval, rightInterval)){
+            return tree[nodePosition].max;
+        }
+        if(intervalIntersectNode(nodePosition, leftInterval , rightInterval)){
+            return Math.max(
+                    getMax(2 * nodePosition, leftInterval, rightInterval),
+                    getMax(2 * nodePosition + 1, leftInterval, rightInterval)
+            );
+        }
+        return Integer.MIN_VALUE;
+    }
+
+    private boolean intervalIntersectNode(int nodePosition, int leftInterval, int rightInterval) {
+        SegmentTreeNode node = tree[nodePosition];
+        return node.leftMost <= rightInterval  ||  leftInterval <= node.rightMost;
+    }
+
+    private boolean isNodeSubsetOfInterval(int nodePosition, int leftInterval, int rightInterval) {
+        SegmentTreeNode node = tree[nodePosition];
+        return leftInterval <= node.leftMost  &&  node.rightMost <= rightInterval;
+    }
+}
+class SegmentTreeNode{
+    int leftMost, rightMost;
+    int max;
+
+    public SegmentTreeNode(int leftMost, int rightMost, Integer number) {
+        this.leftMost = leftMost;
+        this.rightMost = rightMost;
+        max = number;
     }
 }
