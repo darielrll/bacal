cefab88bc06e4159d6081a91d02d22bc97cba24a
Dariel <leiradrll@gmail.com>
2025-06-16 19:30:45 -0400

    refactor

--- src/main/java/drll/problems/leetcode/number_of_valid_clock_times/Solution.java
+++ src/main/java/drll/problems/leetcode/number_of_valid_clock_times/Solution.java
@@ -3,37 +3,34 @@ package drll.problems.leetcode.number_of_valid_clock_times;
 public class Solution {
     public int countTime(String time) {
         String[] splitTime = time.split(":");
-        String hours = splitTime[0];
 
-        int validHoursTimes = 1;
-
-        if(hours.charAt(0) == '?'){
-            if(hours.charAt(1) == '?'){
-                validHoursTimes = 3;
-            }
-            else{
-                int hour = Integer.parseInt(String.valueOf(hours.charAt(1)));
-                validHoursTimes = hour < 4 ? 3 : 2;
-            }
-        }
-        if(hours.charAt(1) == '?'){
-            if(hours.charAt(0) == '?'){
-                validHoursTimes = 24;
-            }
-            else{
-                validHoursTimes = hours.charAt(0) == '0' || hours.charAt(0) == '1' ? 10 : 4;
-            }
-        }
-
-        return validHoursTimes * getValidMinutesTimes(splitTime[1]);
+        String hour = splitTime[0];
+        String minute = splitTime[1];
+        return getValidHoursTimes(hour.charAt(0), hour.charAt(1)) *
+                getValidMinutesTimes(minute.charAt(0), minute.charAt(1));
     }
 
-    private static int getValidMinutesTimes(String minutes) {
+    private static int getValidHoursTimes(char leftDigitHour, char rightDigitHour) {
+        int validHoursTimes = 1;
+
+        if (leftDigitHour == '?') {
+            if (rightDigitHour == '?') {
+                validHoursTimes = 24;
+            } else {
+                validHoursTimes = Integer.parseInt(String.valueOf(rightDigitHour)) > 3 ? 2 : 3;
+            }
+        } else if (rightDigitHour == '?') {
+            validHoursTimes = Integer.parseInt(String.valueOf(leftDigitHour)) < 2 ? 10 : 4;
+        }
+        return validHoursTimes;
+    }
+
+    private static int getValidMinutesTimes(char leftDigitMinute, char rightDigitMinute) {
         int validMinutesTimes = 1;
-        if(minutes.charAt(0) == '?'){
+        if(leftDigitMinute == '?'){
             validMinutesTimes = 6;
         }
-        if(minutes.charAt(1) == '?'){
+        if(rightDigitMinute == '?'){
             validMinutesTimes *= 10;
         }
         return validMinutesTimes;
